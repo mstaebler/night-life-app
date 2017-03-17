be5745d190fe3fbcfe39225143e2c0c9081e1caf
Mark Staebler <markstaebler@gmail.com>
2017-03-17 15:47:33 -0700

    updated user controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,20 +4,17 @@ require('dotenv').config();
 
 module.exports = userRouter;
 
-userRouter.get('/bar/:yelp_id/users', (req, res, next) => {
-    if(req.user)
-        return locations.update({ 
+userRouter.get('/bar/:yelp_id/users', (req, res, next) => {  
+    return locations.find({id: req.params.yelp_id}).then(doc => res.json(doc));
+})
+
+userRouter.post('/users/:yelp_id', (req, res, next) => {
+    return locations.update({ 
             id: req.params.yelp_id, 
             going: req.user.id
         }).then(doc => res.json(doc));
-    else 
-        return res.status(401).json({error:'please log in'})
-})
-
-userRouter.post('/users/:user_id/:yelp_id', (req, res, next) => {
-
 })
 
 userRouter.delete('/users/:user_id/:yelp_id', (req, res, next) => {
-
+    return locations.delete({id: req.params.yelp_id, going: req.params.user_id}).then(doc => res.json(doc));
 })
